83e285cf13e1a611f69b6671c4bc6536313cf104
Henry Owens <henry.tom.owens@gmail.com>
2021-06-22 22:06:42 +0200

    Fix

--- types/index.d.ts
+++ types/index.d.ts
@@ -5,3 +5,9 @@ declare const vueConsoleComponent: Exclude<Plugin["install"], undefined>;
 export default vueConsoleComponent;
 
 export const Console: DefineComponent<{}, {}, any>;
+
+declare module "*.vue" {
+  import type { DefineComponent } from "vue";
+  const component: DefineComponent<{}, {}, any>;
+  export default component;
+}
