2f8a201b63adfa19e5d37901d73e4036a7b4bb3a
Henry Owens <henry.tom.owens@gmail.com>
2021-06-22 21:56:06 +0200

    Fix to compoennt export

--- types/index.d.ts
+++ types/index.d.ts
@@ -4,4 +4,10 @@ declare const vueConsoleComponent: Exclude<Plugin["install"], undefined>;
 
 export default vueConsoleComponent;
 
-export const Console: Exclude<Plugin["install"], undefined> | DefineComponent;
+// export const Console: Exclude<Plugin["install"], undefined> | DefineComponent;
+
+declare module "vue-console-component" {
+  import type { DefineComponent } from "vue";
+  const Console: DefineComponent<{}, {}, any>;
+  export { Console };
+}
